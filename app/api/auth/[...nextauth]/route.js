9985ff654f5b9765a16fc8adca4e423f7d35c586
Adriel <adrielkotorri9@gmail.com>
2025-03-28 19:42:18 +0100

    minimalistic route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,76 +3,24 @@ import TwitterProvider from "next-auth/providers/twitter";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { db } from "../../../lib/firebase-admin";
 
-if (!process.env.NEXTAUTH_SECRET) {
-  throw new Error("Please provide NEXTAUTH_SECRET environment variable");
-}
-
 export const authOptions = {
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
       version: "2.0",
-      profile(profile) {
-        return {
-          id: profile.data.id,
-          name: profile.data.name,
-          email: null,
-          image: profile.data.profile_image_url,
-          username: profile.data.username,
-        };
-      },
     }),
   ],
   adapter: FirestoreAdapter(db),
   secret: process.env.NEXTAUTH_SECRET,
-  debug: true,
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-    updateAge: 24 * 60 * 60, // 24 hours
-  },
-  pages: {
-    signIn: "/", // Redirect to home page for sign in
-  },
   callbacks: {
-    async jwt({ token, account, profile }) {
-      if (account && profile) {
-        token.id = profile.id;
-        token.username = profile.username;
-        token.image = profile.image;
-      }
-      return token;
-    },
     async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id;
+      if (token) {
+        session.user.id = token.sub;
         session.user.username = token.username;
-        session.user.image = token.image;
+        session.user.image = token.picture;
       }
       return session;
-    },
-    async signIn({ account, profile }) {
-      try {
-        console.log("SignIn Callback - Account:", account);
-        console.log("SignIn Callback - Profile:", profile);
-
-        if (account?.provider === "twitter") {
-          return true;
-        }
-        return false;
-      } catch (error) {
-        console.error("Error in signIn callback:", error);
-        return false;
-      }
-    },
-  },
-  events: {
-    async signIn(message) {
-      console.log("User signed in:", message);
-    },
-    async signOut(message) {
-      console.log("User signed out:", message);
     },
   },
 };
